e495ba9300d67f3bca90ff78f2b4061102ca30c5
chenkun <rt5154@totai.com>
2020-08-24 16:25:24 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="container">
+	</view>
+</template>
+
+<script>
+	export default {
+		onPullDownRefresh() {
+			console.log('刷新');
+			setTimeout(function() {
+				uni.stopPullDownRefresh();
+			}, 2000);
+		},
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
